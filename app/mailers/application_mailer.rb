ca82ff04ad24eee80adc540d66637a9cd0bdcf43
Vincent Guidry <vincent.guidry@tinroofsoftware.com>
2025-03-11 11:37:44 -0400

    changing logger in ApplicationMailer to log before rather than after delivery

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,11 +4,11 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'PerfectlyOkay Community Locator <communitylocator@perfectlyokay.org>'
   layout 'bootstrap-mailer'
 
-  after_deliver :log_email
+  before_deliver :log_email
 
   private
 
   def log_email
-    Rails.logger.info("Email sent to #{mail.to} with subject '#{mail.subject}'")
+    Rails.logger.info("Sending email to #{mail.to} from #{mail.from} with subject '#{mail.subject}'")
   end
 end
