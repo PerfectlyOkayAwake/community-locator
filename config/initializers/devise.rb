81cc573a36947b578d378714392fb151cf110865
Vincent Guidry <vincent.guidry@tinroofsoftware.com>
2025-03-12 09:54:46 -0400

    .

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Simply Always Awake Community Locator <communitylocator@simplyalwaysawake.com>'
+  config.mailer_sender = 'PerfectlyOkay Community Locator <communitylocator@perfectlyokay.org>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
